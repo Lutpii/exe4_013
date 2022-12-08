a61c7f8719e1ed19e3dfc92a21bddc5c9873d0c6
Adi Luthfi Nur Roki <adiluthfi03@gmail.com>
2022-12-08 16:09:06 +0700

    create while and switch case for display the method

--- exe4_013/Program.cs
+++ exe4_013/Program.cs
@@ -27,7 +27,7 @@ namespace exe4_013
             else
             {
                 top++;
-                Console.Write("Pushed element is: ");
+                Console.Write("\nPushed element is: ");
                 adi[top] = Console.ReadLine();
             }
         }
@@ -36,12 +36,12 @@ namespace exe4_013
         {
             if (top == -1)
             {
-                Console.WriteLine("Stack Underflow");
+                Console.WriteLine("\nStack Underflow");
                 return -1;
             }
             else
             {
-                Console.Write("Poped element is: " + adi[top]);
+                Console.Write("\nPoped element is: " + adi[top]);
                 top--;
                 return top;
             }
@@ -51,14 +51,14 @@ namespace exe4_013
         {
             if (top == -1)
             {
-                Console.WriteLine("Stack is Empty");
+                Console.WriteLine("\nStack is Empty");
                 return;
             }
             else
             {
                 for (int i = 0; i <= top; i++)
                 {
-                    Console.WriteLine("Item[" + (i + 1) + "]: " + adi[i]);
+                    Console.WriteLine("\nItem[" + (i + 1) + "]: " + adi[i]);
                 }
             }
         }
@@ -68,6 +68,36 @@ namespace exe4_013
         static void Main()
         {
             Stack S = new Stack();
+
+            while (true)
+            {
+                Console.WriteLine();
+                Console.WriteLine("\n**Stack Menu**\n");
+                Console.WriteLine("1. Push ");
+                Console.WriteLine("2. Pop ");
+                Console.WriteLine("3. Display ");
+                Console.WriteLine("4. Exit ");
+                Console.Write("\nEnter your choice: ");
+                string sInput = Console.ReadLine();
+                char ch = Convert.ToChar(sInput == "" ? "0" : sInput);
+                switch (ch)
+                {
+                    case '1':
+                        S.push();
+                        break;
+                    case '2':
+                        S.pop();
+                        break;
+                    case '3':
+                        S.display();
+                        break;
+                    case '4':
+                        return;
+                    default:
+                        Console.WriteLine("\nInvalid Choice");
+                        break;
+                }
+            }
         }
     }
 }
